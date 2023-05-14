eeb84ddc1aab3f8cedba9a5a6ee4368193cf7e33
Heloisa Oliveira <heloisa.oliveira@sou.inteli.edu.br>
2023-05-14 19:03:55 -0300

    Entrega

---
+++ SEMANA_04/02_AUT_EST_ENTREGA/exercicio2.js
@@ -0,0 +1,174 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = './banco_de_dados.db';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+app.use(express.json());
+
+// Retorna todos registros da tabela de Formação (é o R do CRUD - Read)
+app.get('/listaformacao', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT * FROM formacao ORDER BY curso';
+        db.all(sql, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/insereformacao', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+    console.log(req.body.curso);
+	sql = "INSERT INTO formacao (curso, duracao) VALUES ('" + req.body.curso + "', '" + req.body.duracao + "');";
+	console.log(sql);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}	
+	});
+	res.write('<p>FORMAÇÃO INSERIDA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Monta o formulário para o update (é o U do CRUD - Update)
+app.get('/atualizaformacao', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "SELECT * FROM formacao WHERE id_formacao="+ req.query.id_formacao;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/atualizaformacao', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "UPDATE formacao SET curso='" + req.body.curso + "', duracao = '" + req.body.duracao + "' WHERE id_formacao='" + req.body.id_formacao + "';";
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	res.write('<p>FORMAÇÃO ATUALIZADA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.get('/removeformacao', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "DELETE FROM formacao WHERE id_formacao='" + req.query.id_formacao + "'";
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.write('<p>FORMAÇÃO REMOVIDA COM SUCESSO!</p><a href="/">VOLTAR</a>');
+		res.end();
+	});
+	db.close(); 
+});
+
+// Retorna todos registros da parte da tabela de Experiência (é o R do CRUD - Read)
+app.get('/listaexperiencia', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT * FROM experiencia ORDER BY empresa';
+        db.all(sql, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+});
+
+// Retorna todos registros da parte da tabela de Habilidades (é o R do CRUD - Read)
+app.get('/listahabilidades', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT * FROM habilidades ORDER BY nome';
+        db.all(sql, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+});
+
+// Retorna todos registros da parte da tabela de Personalidade (é o R do CRUD - Read)
+app.get('/listapersonalidade', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT * FROM personalidade ORDER BY nome';
+        db.all(sql, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+});
+
+// Retorna todos registros da parte da tabela de Realizações (é o R do CRUD - Read)
+app.get('/listarealizacoes', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT * FROM realizacoes ORDER BY titulo';
+        db.all(sql, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+});
+
+// Retorna todos registros da parte da tabela de Pessoas (é o R do CRUD - Read)
+app.get('/listapessoas', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    var db = new sqlite3.Database(DBPATH);
+    var sql = 'SELECT * FROM pessoas ORDER BY telefone';
+        db.all(sql, [], (err, rows) => {
+            if(err) {
+                throw err;
+            }
+            res.json(rows);
+        });
+        db.close();
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${port}/`)
+});
